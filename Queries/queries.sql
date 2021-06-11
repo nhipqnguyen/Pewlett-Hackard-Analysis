ab35936872297b27c9ff1a1c43c311c4591ba166
Nhi Nguyen <NNguyen@ocjones.com>
2021-06-11 12:25:37 -0700

    complete online lessons

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -99,3 +99,79 @@ LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+-- Create table emp_info of retiring employees containing 
+-- their unique employee number, name, gender, salary and to_date
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+	INNER JOIN salaries as s
+		ON (e.emp_no = s.emp_no)
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
+	 
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+-- List of department retirees
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- List of retirees of the Sales team
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO sales_info
+FROM current_emp as ce 
+LEFT JOIN dept_emp as de
+	ON ce.emp_no = de.emp_no
+LEFT JOIN departments as d
+	ON de.dept_no = d.dept_no
+WHERE de.dept_no = 'd007'
+ORDER BY emp_no;
+
+-- List of retirees of the Sales team and Development team
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO sales_dev_info
+FROM current_emp as ce 
+LEFT JOIN dept_emp as de
+	ON ce.emp_no = de.emp_no
+LEFT JOIN departments as d
+	ON de.dept_no = d.dept_no
+WHERE de.dept_no IN ('d007', 'd005')
+ORDER BY emp_no;
