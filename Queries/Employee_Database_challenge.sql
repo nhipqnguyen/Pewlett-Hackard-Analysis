3db6708c7aaee78398a63d183768bbfb62f009ed
Nhi Nguyen <NNguyen@ocjones.com>
2021-06-18 11:26:26 -0700

    completed challenge queries

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,50 @@
+-- Create a list of retirees with their titles
+SELECT 	e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows 
+-- to get only ythe most recent title of each employee
+SELECT DISTINCT ON (emp_no) rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY emp_no, to_date DESC;
+
+-- Retrieve the number of retirees by their most recent job title
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(emp_no) DESC;
+
+-- Create a Mentorship Eligibility table holding current employees 
+-- born between 1/165 & 12/31/65
+SELECT DISTINCT ON (emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON e.emp_no = de.emp_no
+	INNER JOIN titles as t
+		ON e.emp_no = t.emp_no
+WHERE (de.to_date = ('9999-01-01'))
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+	
